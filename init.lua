b950ae092809a1c683a1158188aade3438e44ff9
kreshnikg <kreshnikg3@gmail.com>
2024-09-28 10:07:17 +0200

    <C-Enter> for autocomplete confirmation

--- init.lua
+++ init.lua
@@ -852,6 +852,7 @@ require('lazy').setup({
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<C-Enter>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
